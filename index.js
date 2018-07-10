ac2f57c83532d0e9f43dcad87de96b0b83f83fa4
fanfengyan <fanfengyan@wecash.net>
2018-07-10 17:44:59 +0800

    some

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 /** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
+import React from 'react'
+import {AppRegistry,NativeModules} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+const{FFTBridge} = NativeModules
 AppRegistry.registerComponent(appName, () => App);
+
+export default class NativeAlert{
+    static showALert(title:string, message:string){
+        FFTBridge.showALert('hello','jack');
+    }
+
+}
